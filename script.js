ae09f6ffb61d7be93700c6ecebfd811e430922b1
BrettMcCannMusic <brettmccann22@gmail.com>
2026-06-17 12:56:21 +0100

    Update script.js
    changed to auto sort gig dates regardless of where they're inputted in index.html

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const gigs = document.querySelectorAll('.gig');
+const gigsSection = document.querySelector('.gigs-section');
+
+/* sort gigs by date */
+
+const sortedGigs = Array.from(gigs).sort((a, b) => {
+  return new Date(a.dataset.date) - new Date(b.dataset.date);
+});
+
+/* put them back in sorted order */
+
+sortedGigs.forEach(gig => {
+  gigsSection.appendChild(gig);
+});
 
 /* remove old gigs */
 
 const today = new Date();
 today.setHours(0, 0, 0, 0);
 
-gigs.forEach((gig) => {
+sortedGigs.forEach((gig) => {
 
   const gigDate = new Date(gig.dataset.date);
   gigDate.setHours(0, 0, 0, 0);
 
-  /* add one full day to gig date */
+  /* remove after gig day finishes */
 
   gigDate.setDate(gigDate.getDate() + 1);
-
-  /* remove once the NEXT day begins */
 
   if (gigDate <= today) {
     gig.remove();
@@ -38,6 +49,6 @@ const observer = new IntersectionObserver((entries) => {
   threshold: 0.15
 });
 
-gigs.forEach((gig) => {
+sortedGigs.forEach((gig) => {
   observer.observe(gig);
 });
